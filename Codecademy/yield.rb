15d5f5736245b57b84e438d1c9b785ee98ad35a6
oched <oched.denis@gmail.com>
2017-09-06 22:09:44 +0300

    06.09.17

--- Codecademy/yield.rb
+++ Codecademy/yield.rb
@@ -7,3 +7,30 @@ puts "Block complete! Back in the method."
 end
 
 yield_name("Eric") { |n| puts "My name is #{n}." }
+
+
+
+
+
+def block_method
+  puts "This is the first line in block_method."
+  yield
+  puts "This statement is after the yield keyword."
+  yield
+  puts "Second after the yield keyword."
+end
+
+block_method do
+  puts "This statement is called from the block."
+end
+
+
+def get_name
+  print "Enter your name: "
+  name = gets.chomp
+  yield name
+end
+
+get_name do |your_name|
+  puts "That's a cool name, #{your_name}!"
+end
